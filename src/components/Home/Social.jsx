c4a36ae3f46b7c6e1ed4567ac30103ea026dadda
unknown <jayrampowar102@gmail.com>
2025-04-18 19:04:02 +0530

    success

--- src/components/Home/Social.jsx
+++ src/components/Home/Social.jsx
@@ -1,7 +1,7 @@
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.linkedin.com/in/developerjayram" className="home__social-icon" target="_blank">
+        <a href="https://www.instagram.com/jayram_powar_1214?igsh=MWo3bGJ4dHFucWc5bA==" className="home__social-icon" target="_blank">
             <i className="uil uil-instagram"></i>
         </a>
         <a href="https://github.com/JayramPowar" className="home__social-icon" target="_blank">
